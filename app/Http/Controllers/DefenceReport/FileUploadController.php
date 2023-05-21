613ab227d20ea91bb0d3ef5fdaa4855753e12ca4
Morski <morski20025@gmail.com>
2023-05-22 02:46:03 +0300

    commit v1.10

--- app/Http/Controllers/DefenceReport/FileUploadController.php
+++ app/Http/Controllers/DefenceReport/FileUploadController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\DefenceReport;
 
 use App\Http\Controllers\Controller;
 use App\Models\DefenceReport;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -17,17 +16,20 @@ class FileUploadController extends Controller
             'file' => 'file'
         ]);
 
-        $data += ['upload_date' => date('d.m.Y H:i')];
+
+        $data += ['upload_date' => date('Y-m-d H:i:s')];
         $data += ['archivist_mark' => 1];
 
+
         try {
+
             $data['file'] = Storage::putFileAs(
                 'public', $data['file'], Str::random(40) . "." . $data['file']->extension()
             );
+
         } catch (\Throwable $e) {
             abort(404);
         }
-
 
 
         $data['file'] = mb_substr($data['file'], 7, 99);
